da3cba5171d3fb1bd30ab5926b43356110da569b
Kummirai <ajaxmilton@hotmail.com>
2024-12-06 20:36:40 +0200

    Add get recipe modal functionality

--- 001-recipe_app/main.js
+++ 001-recipe_app/main.js
@@ -24,14 +24,23 @@ const getRecipe = (letter) => {
           <button data-info="${recipe.idMeal}" class="recipe">Get Recipe</button>
         </div>`;
 
-        modalCard = `
+
+
+        recipeButtons.forEach((button) => {
+          button.addEventListener('click', () => {
+            const mealId = button.getAttribute('data-info');
+            console.log(mealId)
+            const myMenu = data.meals.filter((menu) => menu
+              .idMeal === mealId);
+            myMenu.map((item) => {
+              modalCard = `
         <div class="modal-inner-container">
           <div class="modal-card-container">
-            <img src=${recipe.strMealThumb} alt=${recipe.strMeal}>
+            <img src=${item.strMealThumb} alt=${item.strMeal}>
             <div class="details">
-              <h2>${recipe.strMeal}</h2>
-              <p class="category">${recipe.strCategory}</p>
-              <p class="area">${recipe.strArea}</p>
+              <h2>${item.strMeal}</h2>
+              <p class="category">${item.strCategory}</p>
+              <p class="area">${item.strArea}</p>
           </div>
           <p class="close-modal">&times;</p>
         </div>
@@ -39,52 +48,48 @@ const getRecipe = (letter) => {
           <h3>Ingredients</h3>
           <div class="ingredients">
             <ul>
-             <li class=${recipe.strIngredient1 === "" ? "hidden" : "" }>${recipe.strIngredient1}</li>
-              <li class=${recipe.strIngredient2 === "" ? "hidden" : "" }>${recipe.strIngredient2}</li>
-              <li class=${recipe.strIngredient3 === "" ? "hidden" : "" }>${recipe.strIngredient3}</li>
-              <li class=${recipe.strIngredient4 === "" ? "hidden" : "" }>${recipe.strIngredient4}</li>
-              <li class=${recipe.strIngredient5 === "" ? "hidden" : "" }>${recipe.strIngredient5}</li>
-              <li class=${recipe.strIngredient6 === "" ? "hidden" : "" }>${recipe.strIngredient6}</li>
-              <li class=${recipe.strIngredient7 === "" ? "hidden" : "" }>${recipe.strIngredient7}</li>
-              <li class=${recipe.strIngredient8 === "" ? "hidden" : "" }>${recipe.strIngredient8}</li>
-              <li class=${recipe.strIngredient9 === "" ? "hidden" : "" }>${recipe.strIngredient9}</li>
-              <li class=${recipe.strIngredient10 === "" ? "hidden" : "" }>${recipe.strIngredient10}</li>
-              <li class=${recipe.strIngredient11 === "" ? "hidden" : "" }>${recipe.strIngredient11}</li>
-              <li class=${recipe.strIngredient12 === "" ? "hidden" : "" }>${recipe.strIngredient12}</li>
-              <li class=${recipe.strIngredient13 === "" ? "hidden" : "" }>${recipe.strIngredient13}</li>
-              <li class=${recipe.strIngredient14 === "" ? "hidden" : "" }>${recipe.strIngredient14}</li>
-              <li class=${recipe.strIngredient15 === "" ? "hidden" : "" }>${recipe.strIngredient15}</li>
-              <li class=${recipe.strIngredient16 === "" ? "hidden" : "" }>${recipe.strIngredient16}</li>
-              <li class=${recipe.strIngredient17 === "" ? "hidden" : "" }>${recipe.strIngredient17}</li>
-              <li class=${recipe.strIngredient18 === "" ? "hidden" : "" }>${recipe.strIngredient18}</li>
-              <li class=${recipe.strIngredient19 === "" ? "hidden" : "" }>${recipe.strIngredient19}</li>
+             <li class=${item.strIngredient1 === "" ? "hidden" : "" }>${item.strIngredient2}</li>
+              <li class=${item.strIngredient2 === "" ? "hidden" : "" }>${item.strIngredient2}</li>
+              <li class=${item.strIngredient3 === "" ? "hidden" : "" }>${item.strIngredient3}</li>
+              <li class=${item.strIngredient4 === "" ? "hidden" : "" }>${item.strIngredient4}</li>
+              <li class=${item.strIngredient5 === "" ? "hidden" : "" }>${item.strIngredient5}</li>
+              <li class=${item.strIngredient6 === "" ? "hidden" : "" }>${item.strIngredient6}</li>
+              <li class=${item.strIngredient7 === "" ? "hidden" : "" }>${item.strIngredient7}</li>
+              <li class=${item.strIngredient8 === "" ? "hidden" : "" }>${item.strIngredient8}</li>
+              <li class=${item.strIngredient9 === "" ? "hidden" : "" }>${item.strIngredient9}</li>
+              <li class=${item.strIngredient10 === "" ? "hidden" : "" }>${item.strIngredient10}</li>
+              <li class=${item.strIngredient11 === "" ? "hidden" : "" }>${item.strIngredient11}</li>
+              <li class=${item.strIngredient12 === "" ? "hidden" : "" }>${item.strIngredient12}</li>
+              <li class=${item.strIngredient13 === "" ? "hidden" : "" }>${item.strIngredient13}</li>
+              <li class=${item.strIngredient14 === "" ? "hidden" : "" }>${item.strIngredient14}</li>
+              <li class=${item.strIngredient15 === "" ? "hidden" : "" }>${item.strIngredient15}</li>
+              <li class=${item.strIngredient16 === "" ? "hidden" : "" }>${item.strIngredient16}</li>
+              <li class=${item.strIngredient17 === "" ? "hidden" : "" }>${item.strIngredient17}</li>
+              <li class=${item.strIngredient18 === "" ? "hidden" : "" }>${item.strIngredient18}</li>
+              <li class=${item.strIngredient19 === "" ? "hidden" : "" }>${item.strIngredient19}</li>
             </ul>
           </div>
           <h3>Cooking Instructions</h3>
           <div class="instructions">
             
-            <p>${recipe.strInstructions}</p>
+            <p>${item.strInstructions}</p>
           </div>
         </div>
         `
-        modal.innerHTML = modalCard;
-        const closeModal = document.querySelector('.close-modal');
-
-        recipeButtons.forEach((button) => {
-          button.addEventListener('click', (id) => {
-            modal.style.display = "block";
-            const mealId = button.getAttribute('data-info');
-            console.log(mealId);
+              modal.innerHTML = modalCard;
+              modal.style.display = "block";
+              const closeModal = document.querySelector(
+                '.close-modal');
+                
+              closeModal.addEventListener('click', () => {
+                modal.style.display = "none";
+              })
+            })
           })
         })
 
-
-        closeModal.addEventListener('click', () => {
-          modal.style.display = "none";
-        })
       })))
     .catch(error => console.log(error))
-
 }
 
 getRecipe("e")
