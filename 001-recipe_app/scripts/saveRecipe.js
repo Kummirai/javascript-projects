7dfd5f3d1ca73566eaf620fdb0a7160b5e0308bc
Kummirai <ajaxmilton@hotmail.com>
2024-12-12 19:17:53 +0200

    add functionality to remove recipe from saved recipes

--- 001-recipe_app/scripts/saveRecipe.js
+++ 001-recipe_app/scripts/saveRecipe.js
@@ -1,7 +1,7 @@
 let savedCard = "";
 
 const savedSection = document.querySelector('.saved-section');
-const myRecipes = JSON.parse(localStorage.getItem('myRecipes'));
+let myRecipes = JSON.parse(localStorage.getItem('myRecipes'));
 
 myRecipes?.map((recipe) => {
   savedCard += `<div class="card-container">
@@ -12,11 +12,30 @@ myRecipes?.map((recipe) => {
                     <p class="area">${recipe.area}</p>
                   </div>
                   <div class="buttons">
-                    <i data-info="${recipe.idMeal}" class=" fas fa-eye recipe save "></i>
-                    <i data-info="${recipe.idMeal}"class="fas fa-trash"></i>
-                    <i data-info="${recipe.idMeal}" class="fas fa-play watch save"></i>
+                    <i data-info="${recipe.id}" class=" fas fa-eye recipe save "></i>
+                    <i data-info="${recipe.id}"class="fas fa-trash"></i>
+                    <i data-info="${recipe.id}" class="fas fa-play watch save"></i>
                   </div>
               </div>`;
 
   savedSection.innerHTML = savedCard;
+
+  const btns = document.querySelectorAll('.fa-trash');
+  btns.forEach((btn) => {
+
+    btn.addEventListener('click', () => {
+      const idForRecipe = btn.getAttribute('data-info');
+      const recipeToDelete = myRecipes.find((recipe) => recipe
+        .id === idForRecipe);
+
+      const index = myRecipes.indexOf(recipeToDelete);
+      myRecipes.splice(index, 1);
+
+      localStorage.setItem('myRecipes', JSON.stringify(myRecipes));
+      
+      alert(`Recipe sucessfully deleted!`)
+      
+      window.location.href = "savedRecipes.html";
+    })
+  })
 })
