3677eb2c7e0e5f16d2bc9af77875dee0f57a2925
Kummirai <ajaxmilton@hotmail.com>
2024-12-12 10:51:05 +0200

    add more styles to account section

--- 001-recipe_app/scripts/recipes.js
+++ 001-recipe_app/scripts/recipes.js
@@ -1,18 +1,30 @@
 const isLogged = document.querySelector('.islogged');
 const logInStatus = localStorage.getItem('logInStatus');
+const savedRecipes = document.querySelector('.savedRecipes');
+
 console.log(logInStatus)
 
 const username = localStorage.getItem('user');
 
 const user = document.querySelector('.user');
 
-if(logInStatus === 'loggedIn'){
+if (logInStatus === 'loggedIn') {
   user.textContent = username;
   isLogged.textContent = "Log out";
 }
 
-isLogged.addEventListener('click', ()=>{
-  if(isLogged.textContent === 'Log out'){
+isLogged.addEventListener('click', () => {
+  if (isLogged.textContent === 'Log out') {
     window.location.href = 'index.html'
+  } else {
+    window.location.href = 'login.html'
+  }
+})
+
+savedRecipes.addEventListener('click', () => {
+  if (isLogged.textContent === 'Log out') {
+    window.location.href = 'savedRecipes.html'
+  } else {
+    window.location.href = "login.html"
   }
 })
