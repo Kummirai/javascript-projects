01a00de47613834d7a223aa19594e9bf9469aa89
Kummirai <ajaxmilton@hotmail.com>
2024-12-18 11:42:34 +0200

    Dynamically change skills section metrics

--- 003-portifolio/main.js
+++ 003-portifolio/main.js
@@ -1,1 +1,20 @@
-document.getElementById("bar1").style.width = "0%";
+const HTML = "90%"; 
+const CSS = "95%";
+const javascript = "85%";
+const react = "80%";
+const git = "95%"; 
+const bootstrap = "90%"; 
+
+document.getElementById("bar1").style.width = HTML;
+document.getElementById("bar2").style.width = CSS;
+document.getElementById("bar3").style.width = javascript;
+document.getElementById("bar4").style.width = react;
+document.getElementById("bar5").style.width = git;
+document.getElementById("bar6").style.width = bootstrap;
+
+document.querySelector(".percent1").innerHTML = HTML;
+document.querySelector(".percent2").innerHTML = CSS;
+document.querySelector(".percent3").innerHTML = javascript;
+document.querySelector(".percent4").innerHTML = react;
+document.querySelector(".percent5").innerHTML = git;
+document.querySelector(".percent6").innerHTML = bootstrap;
