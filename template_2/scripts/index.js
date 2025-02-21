91041f0740b82766c7b1391f697ece6e61e0ae7f
Kummirai <ajaxmilton@hotmail.com>
2025-02-21 16:36:30 +0200

    Add functionality to main page

--- template_2/scripts/index.js
+++ template_2/scripts/index.js
@@ -14,36 +14,42 @@ const burgers = [
     title: "Tripple Chiz Burger",
     image: "images/hamburger.png",
     desc: "Savor the perfect crispy-edged, juicy smash burger, layered with melted American cheese, caramelized onions, and tangy house sauce on a buttery toasted brioche bun. A bite of pure satisfaction awaits!",
+    price: "$5.99",
   },
   {
     id: 2,
     title: "D-Tripple Burger",
     image: "images/product2.png",
     desc: "Twice the beef, twice the cheese, and double the flavor! Our handcrafted double cheeseburger is stacked with premium Angus beef, melted cheddar, and crisp lettuce for a mouthwatering experience",
+    price: "$4.99",
   },
   {
     id: 3,
     title: "Spicy Inferno Burger",
     image: "images/hamburger.png",
     desc: "Heat lovers, meet your match! Our Spicy Inferno Burger packs a fiery punch with jalapeños, pepper jack cheese, and smoky chipotle mayo on a toasted sesame bun. Dare to take a bite?",
+    price: "$5.99",
   },
   {
     id: 4,
     title: "BBQ Bourbon Bliss Burger",
     image: "images/product2.png",
     desc: "Smoky and savory—this BBQ Bourbon Bliss Burger is a flavor explosion! Hickory-smoked bacon, tangy bourbon BBQ sauce, crispy onion straws, and melted cheddar bring backyard BBQ vibes to every bite.",
+    price: "$6.50",
   },
   {
     id: 5,
     title: "Mushroom Swiss Burger",
     image: "images/product1.png",
     desc: "Indulge in elegance with our Truffle Mushroom Swiss Burger. Juicy beef meets sautéed mushrooms, Swiss cheese, and a hint of truffle aioli for a gourmet twist on a classic favorite.",
+    price: "$7.99",
   },
   {
     id: 6,
     title: "Ultimate Veggie Burger",
     image: "images/hamburger.png",
     desc: "Plant-based perfection! Our Ultimate Veggie Burger is crafted with a hearty black bean patty, crisp greens, fresh avocado, and zesty vegan aioli for a guilt-free yet deliciously satisfying bite.",
+    price: "$4.50",
   },
 ];
 
@@ -71,10 +77,16 @@ const burgerSection = () => {
       myBurgers[prevPosition].classList.remove("opacity");
       mainProductDesc.innerHTML = burgers[initialPosition].desc;
       mainProduct.innerHTML = burgers[initialPosition].title;
+      mainProductPrice.innerHTML = burgers[initialPosition].price;
+      mainProductImg.src = burgers[initialPosition].image;
+      nextProductImg.src = burgers[initialPosition - 1].image;
     } else if (initialPosition === burgerItems) {
       initialPosition = 0;
       mainProductDesc.innerHTML = burgers[initialPosition].desc;
       mainProduct.innerHTML = burgers[initialPosition].title;
+      mainProductPrice.innerHTML = burgers[initialPosition].price;
+      mainProductImg.src = burgers[initialPosition].image;
+
       myBurgers[initialPosition].classList.add("opacity");
       myBurgers[burgerItems - 1].classList.remove("opacity");
     }
@@ -86,10 +98,19 @@ const burgerSection = () => {
       myBurgers[initialPosition].classList.add("opacity");
       const prevPosition = initialPosition + 1;
       myBurgers[prevPosition].classList.remove("opacity");
+      mainProductDesc.innerHTML = burgers[initialPosition].desc;
+      mainProduct.innerHTML = burgers[initialPosition].title;
+      mainProductPrice.innerHTML = burgers[initialPosition].price;
+      mainProductImg.src = burgers[initialPosition].image;
+      nextProductImg.src = burgers[initialPosition + 1].image;
     } else if (initialPosition < 0) {
       initialPosition = burgerItems - 1;
       myBurgers[initialPosition].classList.add("opacity");
       myBurgers[0].classList.remove("opacity");
+      mainProductDesc.innerHTML = burgers[initialPosition].desc;
+      mainProduct.innerHTML = burgers[initialPosition].title;
+      mainProductPrice.innerHTML = burgers[initialPosition].price;
+      mainProductImg.src = burgers[initialPosition].image;
     }
   });
 };
