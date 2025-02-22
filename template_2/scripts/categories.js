6270be63e8195c67cc1592c318e7a50f55115e8e
Kummirai <ajaxmilton@hotmail.com>
2025-02-22 21:13:33 +0200

    Add categories section content and styles

---
+++ template_2/scripts/categories.js
@@ -0,0 +1,53 @@
+const category = document.querySelector(".categories");
+
+const categories = [
+  {
+    img: "images/product2.png",
+    title: "Chicken",
+  },
+  {
+    img: "images/product2.png",
+    title: "Burger",
+  },
+  {
+    img: "images/product2.png",
+    title: "",
+  },
+  {
+    img: "images/product2.png",
+    title: "Pizza",
+  },
+  {
+    img: "images/product2.png",
+    title: "Fries",
+  },
+  {
+    img: "images/product2.png",
+    title: "Burger Meal",
+  },
+  {
+    img: "images/product2.png",
+    title: "Chesse Burger",
+  },
+  {
+    img: "images/product2.png",
+    title: "Chips n Chicken",
+  },
+];
+
+let categoryArticle = "";
+
+const categorySection = () => {
+  categories.map((article) => {
+    categoryArticle += `
+             <article>
+              <img src=${article.img} alt=${article.title} />
+              <h3>${article.title}</h3>
+            </article>
+    `;
+  });
+  console.log(categoryArticle);
+  category.innerHTML = categoryArticle;
+};
+
+categorySection();
--- template_2/scripts/index.js
+++ template_2/scripts/index.js
@@ -121,3 +121,5 @@ const burgerSection = () => {
 };
 
 burgerSection();
+
+
