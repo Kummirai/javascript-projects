9ae80bc1c350e241f79e370f21a3cb8659dd9ca0
Kummirai <ajaxmilton@hotmail.com>
2025-02-23 19:23:35 +0200

    Add order calculation

--- template_2/scripts/categories.js
+++ template_2/scripts/categories.js
@@ -1,5 +1,7 @@
 const category = document.querySelector(".categories");
 const products = document.querySelector(".products");
+const invoiceContent = document.querySelector(".invoice-content");
+const mySubTotal = document.querySelector(".subtotal");
 
 const categories = [
   {
@@ -59,42 +61,42 @@ const burgers = [
     title: "Tripple Chiz Burger",
     image: "images/product6.png",
     desc: "Savor the perfect crispy-edged, juicy smash burger, layered with melted American cheese, caramelized onions, and tangy house sauce on a buttery toasted brioche bun. A bite of pure satisfaction awaits!",
-    price: "$5.99",
+    price: 599,
   },
   {
     id: 2,
     title: "D-Tripple Burger",
     image: "images/product4.png",
     desc: "Twice the beef, twice the cheese, and double the flavor! Our handcrafted double cheeseburger is stacked with premium Angus beef, melted cheddar, and crisp lettuce for a mouthwatering experience",
-    price: "$4.99",
+    price: 499,
   },
   {
     id: 3,
     title: "Spicy Inferno Burger",
     image: "images/product3.png",
     desc: "Heat lovers, meet your match! Our Spicy Inferno Burger packs a fiery punch with jalapeños, pepper jack cheese, and smoky chipotle mayo on a toasted sesame bun. Dare to take a bite?",
-    price: "$5.99",
+    price: 599,
   },
   {
     id: 4,
     title: "BBQ Bliss Burger",
     image: "images/product2.png",
     desc: "Smoky and savory—this BBQ Bourbon Bliss Burger is a flavor explosion! Hickory-smoked bacon, tangy bourbon BBQ sauce, crispy onion straws, and melted cheddar bring backyard BBQ vibes to every bite.",
-    price: "$6.50",
+    price: 650,
   },
   {
     id: 5,
     title: "Mushroom Swiss Burger",
     image: "images/product1.png",
     desc: "Indulge in elegance with our Truffle Mushroom Swiss Burger. Juicy beef meets sautéed mushrooms, Swiss cheese, and a hint of truffle aioli for a gourmet twist on a classic favorite.",
-    price: "$7.99",
+    price: 799,
   },
   {
     id: 6,
     title: "Ultimate Veggie Burger",
     image: "images/product5.png",
     desc: "Plant-based perfection! Our Ultimate Veggie Burger is crafted with a hearty black bean patty, crisp greens, fresh avocado, and zesty vegan aioli for a guilt-free yet deliciously satisfying bite.",
-    price: "$4.50",
+    price: 450,
   },
 ];
 
@@ -108,17 +110,69 @@ const productSection = () => {
           <img src=${product.image} alt=${product.title} />
         </div>
         <h2>${product.title}</h2>
-        <h3>${product.price}</h3>
+        <h3>$${product.price / 100}</h3>
         <div class="buttons">
           <button class="btn wishlist">Wishlist</button>
-          <button class="btn order-now">Order now</button>
+          <button data-burger-image=${product.image} data-burger-price=${
+      product.price
+    } data-burger-title=${
+      product.title
+    } class="btn order-now">Order now</button>
         </div>
       </article>
     `;
   });
-  console.log(productArticle);
 
   products.innerHTML = productArticle;
+  let customerOrder = [];
+  let orderHtml = "";
+
+  const orderButtons = document.querySelectorAll(".order-now");
+  orderButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      invoiceContent.innerHTML = "";
+      const image = button.getAttribute("data-burger-image");
+      const title = button.getAttribute("data-burger-title");
+      const price = button.getAttribute("data-burger-price");
+
+      const ordered = {
+        name: title,
+        img: image,
+        cost: price,
+      };
+
+      customerOrder = [...customerOrder, ordered];
+      let subtotal = 0;
+      customerOrder.map((price) => {
+        subtotal = subtotal + Number(price.cost);
+      });
+
+      mySubTotal.innerHTML = `$${(subtotal / 100).toFixed(2)}`;
+
+      invoiceContent.innerHTML = "";
+      orderHtml = "";
+      customerOrder.map((order) => {
+        orderHtml += `
+              <article>
+                <div class="product">
+                  <img src=${order.img} alt=${order.titie} />
+                  <div>
+                    <h2>${order.name}</h2>
+                    <h3>$${(order.cost / 100).toPrecision(3)}</h3>
+                  </div>
+                </div>
+                <div class="quantity">
+                  <i class="fas fa-minus"></i>
+                  <p>1</p>
+                  <i class="fas fa-plus"></i>
+                </div>
+              </article>
+          `;
+      });
+
+      invoiceContent.insertAdjacentHTML("afterbegin", orderHtml);
+    });
+  });
 };
 
 productSection();
