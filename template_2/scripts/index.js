46d0322523acfb5f8206929539fbe59f4fecc192
Kummirai <ajaxmilton@hotmail.com>
2025-02-23 08:55:10 +0200

    Add product images

--- template_2/scripts/index.js
+++ template_2/scripts/index.js
@@ -12,21 +12,21 @@ const burgers = [
   {
     id: 1,
     title: "Tripple Chiz Burger",
-    image: "images/hamburger.png",
+    image: "images/product6.png",
     desc: "Savor the perfect crispy-edged, juicy smash burger, layered with melted American cheese, caramelized onions, and tangy house sauce on a buttery toasted brioche bun. A bite of pure satisfaction awaits!",
     price: "$5.99",
   },
   {
     id: 2,
     title: "D-Tripple Burger",
-    image: "images/product3.png",
+    image: "images/product4.png",
     desc: "Twice the beef, twice the cheese, and double the flavor! Our handcrafted double cheeseburger is stacked with premium Angus beef, melted cheddar, and crisp lettuce for a mouthwatering experience",
     price: "$4.99",
   },
   {
     id: 3,
     title: "Spicy Inferno Burger",
-    image: "images/hamburger.png",
+    image: "images/product3.png",
     desc: "Heat lovers, meet your match! Our Spicy Inferno Burger packs a fiery punch with jalapeños, pepper jack cheese, and smoky chipotle mayo on a toasted sesame bun. Dare to take a bite?",
     price: "$5.99",
   },
@@ -47,7 +47,7 @@ const burgers = [
   {
     id: 6,
     title: "Ultimate Veggie Burger",
-    image: "images/product2.png",
+    image: "images/product5.png",
     desc: "Plant-based perfection! Our Ultimate Veggie Burger is crafted with a hearty black bean patty, crisp greens, fresh avocado, and zesty vegan aioli for a guilt-free yet deliciously satisfying bite.",
     price: "$4.50",
   },
