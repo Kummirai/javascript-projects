948809bf00580bdffbf6c552005fd33cf337c78e
Kummirai <ajaxmilton@hotmail.com>
2025-03-02 12:38:40 +0200

    Optimize images

--- template_2/scripts/categories.js
+++ template_2/scripts/categories.js
@@ -5,35 +5,35 @@ const mySubTotal = document.querySelector(".subtotal");
 
 const categories = [
   {
-    img: "images/product1.png",
+    img: "images/product1.webp",
     title: "Chicken",
   },
   {
-    img: "images/product2.png",
+    img: "images/product2.webp",
     title: "Burger",
   },
   {
-    img: "images/product3.png",
+    img: "images/product3.webp",
     title: "Chicken",
   },
   {
-    img: "images/product4.png",
+    img: "images/product4.webp",
     title: "Pizza",
   },
   {
-    img: "images/product5.png",
+    img: "images/product5.webp",
     title: "Fries",
   },
   {
-    img: "images/product6.png",
+    img: "images/product6.webp",
     title: "Burger Meal",
   },
   {
-    img: "images/hamburger.png",
+    img: "images/hamburger.webp",
     title: "Chesse Burger",
   },
   {
-    img: "images/product2.png",
+    img: "images/product2.webp",
     title: "Chips n Chicken",
   },
 ];
@@ -59,42 +59,42 @@ const burgers = [
   {
     id: 1,
     title: "Tripple Chiz Burger",
-    image: "images/product6.png",
+    image: "images/product6.webp",
     desc: "Savor the perfect crispy-edged, juicy smash burger, layered with melted American cheese, caramelized onions, and tangy house sauce on a buttery toasted brioche bun. A bite of pure satisfaction awaits!",
     price: 599,
   },
   {
     id: 2,
     title: "D-Tripple Burger",
-    image: "images/product4.png",
+    image: "images/product4.webp",
     desc: "Twice the beef, twice the cheese, and double the flavor! Our handcrafted double cheeseburger is stacked with premium Angus beef, melted cheddar, and crisp lettuce for a mouthwatering experience",
     price: 499,
   },
   {
     id: 3,
     title: "Spicy Inferno Burger",
-    image: "images/product3.png",
+    image: "images/product3.webp",
     desc: "Heat lovers, meet your match! Our Spicy Inferno Burger packs a fiery punch with jalapeños, pepper jack cheese, and smoky chipotle mayo on a toasted sesame bun. Dare to take a bite?",
     price: 599,
   },
   {
     id: 4,
     title: "BBQ Bliss Burger",
-    image: "images/product2.png",
+    image: "images/product2.webp",
     desc: "Smoky and savory—this BBQ Bourbon Bliss Burger is a flavor explosion! Hickory-smoked bacon, tangy bourbon BBQ sauce, crispy onion straws, and melted cheddar bring backyard BBQ vibes to every bite.",
     price: 650,
   },
   {
     id: 5,
     title: "Mushroom Swiss Burger",
-    image: "images/product1.png",
+    image: "images/product1.webp",
     desc: "Indulge in elegance with our Truffle Mushroom Swiss Burger. Juicy beef meets sautéed mushrooms, Swiss cheese, and a hint of truffle aioli for a gourmet twist on a classic favorite.",
     price: 799,
   },
   {
     id: 6,
     title: "Ultimate Veggie Burger",
-    image: "images/product5.png",
+    image: "images/product5.webp",
     desc: "Plant-based perfection! Our Ultimate Veggie Burger is crafted with a hearty black bean patty, crisp greens, fresh avocado, and zesty vegan aioli for a guilt-free yet deliciously satisfying bite.",
     price: 450,
   },
@@ -133,7 +133,7 @@ const productSection = () => {
       invoiceContent.innerHTML = "";
       const image = button.getAttribute("data-burger-image");
       const title = button.getAttribute("data-burger-title");
-      const price = button.getAttribute("data-burger-price");
+      const price = button.getAttribute("data-burger-pricewebp
 
       const ordered = {
         name: title,
